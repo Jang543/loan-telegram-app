ec26b068bf84115c7dfb7c210e90f3d8e6250c86
Jang543 <noway07788@gmail.com>
2026-09-22 01:53:56 +0500

    Update server.js

--- server.js
+++ server.js
@@ -68,4 +68,4 @@ app.post('/submit', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
