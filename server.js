1f0ce9093f39bf4dbf7a5b27a122e0c01e3fafd9
Jang543 <noway07788@gmail.com>
2026-09-22 23:04:51 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;8898828564:AAHLazmrHkhpvrf1jBZv5C404lo96n7s400
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;8988726172:AAH3LjtRaeBTSMqrhmQi6QV0L1XiYhQn3aU
 const CHAT_ID = process.env.TELEGRAM_CHAT_ID;8575428267
 
 // Sirf safe application fields
